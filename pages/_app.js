3b0fb9ccbac7ebfefcda84bb42fe0d0a015b212b
Todd Mpeli <toddowenmpeli@rocketmail.com>
2022-11-06 13:48:21 +0000

    Removed initial copied default flies and folders. To have a bare bones project.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import "../styles/globals.scss";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function MyApp({Component, pageProps}) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			{/* <!--===== NAVIGATION =====--> */}
+			<Navbar />
+
+			<Component {...pageProps} />
+
+			{/* <!--===== FOOTER =====--> */}
+			<Footer />
+		</>
+	);
 }
 
 export default MyApp;
