31e0fd76f4a0ddb94a7348b9005586d52b100fa4
Todd Mpeli <toddowenmpeli@rocketmail.com>
2022-11-13 23:47:38 +0000

    Removed the navigation section from the hero component into the Navbar component and created a 404 error page component. Also added new scss colour variables.

--- components/Hero.js
+++ components/Hero.js
@@ -1,103 +1,10 @@
 import Image from "next/image";
+import Navbar from "../components/Navbar";
 import styles from "../components/Hero.module.scss";
 
 const Hero = () => {
 	return (
 		<section className={styles.hero}>
-			<nav className="navigation py-1 bg-white">
-				<div className="container mx-auto px-4">
-					<div className="relative flex items-center justify-between">
-						<div className="w-auto">
-							<a className="inline-block" href="#">
-								<Image
-									src="/img/Logos/BlueInventory Logo One White.png"
-									alt=""
-									width="185px"
-									height="70px"
-									objectFit="fit-content"
-								/>
-							</a>
-						</div>
-						<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hidden lg:block">
-							<ul className="flex items-center">
-								<li className="font-heading mr-12 text-base">
-									<a
-										className="hover:text-black hover:text-grey"
-										href="#"
-										contentEditable="false"
-									>
-										Features
-									</a>
-								</li>
-								<li className="font-heading mr-12 text-base">
-									<a
-										className="hover:text-black hover:text-grey"
-										href="#"
-										contentEditable="false"
-									>
-										About
-									</a>
-								</li>
-								<li className="font-heading mr-12 text-base">
-									<a
-										className="hover:text-black hover:text-grey"
-										href="#"
-										contentEditable="false"
-									>
-										Pricing
-									</a>
-								</li>
-								<li className="font-heading mr-12 text-base">
-									<a
-										className="hover:text-black hover:text-grey"
-										href="#"
-										contentEditable="false"
-									>
-										Resources
-									</a>
-								</li>
-							</ul>
-						</div>
-						<div className="hidden lg:flex flex-row gap-6 w-auto">
-							<a
-								className="inline-block py-2 px-4 rounded-[15px] font-heading text-sm text-white border hover:ease-in-out hover:duration-200 bg-darkBlue hover:bg-blue"
-								href="#"
-							>
-								Login
-							</a>
-							<a
-								className="inline-block py-2 px-4 rounded-[15px] font-heading text-sm text-white border hover:ease-in-out hover:duration-200 bg-blue hover:bg-limeGreen"
-								href="#"
-							>
-								Sign Up
-							</a>
-						</div>
-						{/* Mobile Navigation Menu */}
-						<div className="w-auto lg:hidden">
-							<a
-								className="navbar-burger inline-flex w-14 h-14 justify-center items-center bg-blue hover:bg-blueTwo rounded-[15px]"
-								href="#"
-							>
-								<svg
-									width="20"
-									height="10"
-									viewBox="0 0 20 10"
-									fill="none"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path
-										d="M19 9H1M19 1H1"
-										stroke="white"
-										strokeWidth="1.5"
-										strokeLinecap="round"
-										strokeLinejoin="round"
-									></path>
-								</svg>
-							</a>
-						</div>
-					</div>
-				</div>
-			</nav>
 			<div className={styles.mainHero}>
 				<div className="pt-24 sm:pt-34 pb-8 bg-cover">
 					<div className="container px-4 mx-auto">
