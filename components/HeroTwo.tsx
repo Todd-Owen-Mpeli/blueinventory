1ca741e0bdae6ac007b296e36b1808d2c5261a22
Todd Mpeli <toddowenmpeli@rocketmail.com>
2023-05-24 21:17:11 +0100

    Mobile Updated Styling tweaks.

--- components/HeroTwo.tsx
+++ components/HeroTwo.tsx
@@ -43,9 +43,9 @@ const HeroTwo: FC<HeroProps> = ({
 
 	return (
 		<section className={styles.hero}>
-			<div className="flex flex-col bg-cover bg-center bg-no-repeat relative h-full min-h-[65vh]">
+			<div className="flex flex-col bg-cover bg-center bg-no-repeat relative h-full min-h-[40vh] sm:min-h-[65vh]">
 				<div
-					className="absolute top-0 bottom-0 left-0 w-full h-full max-h-[65vh] z-[995] bg-center bg-no-repeat bg-cove"
+					className="absolute top-0 bottom-0 left-0 w-full h-full max-h-[40vh] sm:max-h-[65vh] z-[995] bg-center bg-no-repeat bg-cove"
 					style={{backgroundImage: `url("${backgroundImage?.sourceUrl}")`}}
 				>
 					{/* Background Video */}
