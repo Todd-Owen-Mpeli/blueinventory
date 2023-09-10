30fabaf72cdc4819e484c9442c1c80a9f9f6f730
Todd Mpeli <toddowenmpeli@rocketmail.com>
2023-09-10 15:26:31 +0100

    Updated general styling

--- components/Frontend/TitleThreeContentGrid.tsx
+++ components/Frontend/TitleThreeContentGrid.tsx
@@ -14,62 +14,69 @@ const TitleThreeContentGrid: FC<ITitleThreeContentGrid> = ({
 	contentGrid,
 }) => {
 	return (
-		<div className="py-12 bg-white lg:px-4 sm:py-28">
-			<div className="container px-4 mx-auto">
-				<motion.div
-					initial={initial}
-					whileInView={stagger}
-					viewport={{once: true}}
-					className="flex flex-wrap items-center justify-center gap-6 pb-12 mb-16 border-b lg:justify-start lg:items-start sm:pb-20 border-grey"
-				>
+		<div className="bg-darkerBlueTwo">
+			<div
+				className="px-4 py-12 bg-white sm:py-28"
+				style={{
+					clipPath: `polygon(0.8% 0%, 100% 0%, 100% 99%, 2.8% 97%)`,
+				}}
+			>
+				<div className="container px-0 mx-auto">
 					<motion.div
 						initial={initial}
-						whileInView={fadeInUp}
+						whileInView={stagger}
 						viewport={{once: true}}
+						className="flex flex-wrap items-center justify-center gap-6 pb-12 mb-16 border-b lg:justify-start lg:items-start sm:pb-20 border-grey"
 					>
-						<Image
-							alt={icon?.altText}
-							src={icon?.sourceUrl}
-							width={icon?.mediaDetails?.width}
-							height={icon?.mediaDetails?.height}
-							className={
-								icon?.sourceUrl
-									? `block w-full max-w-[125px] h-[100px] object-contain object-center`
-									: `hidden`
-							}
-						/>
+						<motion.div
+							initial={initial}
+							whileInView={fadeInUp}
+							viewport={{once: true}}
+						>
+							<Image
+								alt={icon?.altText}
+								src={icon?.sourceUrl}
+								width={icon?.mediaDetails?.width}
+								height={icon?.mediaDetails?.height}
+								className={
+									icon?.sourceUrl
+										? `block w-full max-w-[125px] h-[100px] object-contain object-center`
+										: `hidden`
+								}
+							/>
+						</motion.div>
+						<motion.h2
+							initial={initial}
+							whileInView={fadeInUp}
+							viewport={{once: true}}
+							className="text-xl font-bold leading-normal xl:leading-[2.75rem] xl:text-6xl tracking-px-n md:max-w-3xl"
+						>
+							{title}
+						</motion.h2>
 					</motion.div>
-					<motion.h2
+					<motion.div
 						initial={initial}
-						whileInView={fadeInUp}
+						whileInView={stagger}
 						viewport={{once: true}}
-						className="text-xl font-bold leading-normal xl:leading-[2.75rem] xl:text-6xl tracking-px-n md:max-w-3xl"
+						className="flex flex-wrap m-8"
 					>
-						{title}
-					</motion.h2>
-				</motion.div>
-				<motion.div
-					initial={initial}
-					whileInView={stagger}
-					viewport={{once: true}}
-					className="flex flex-wrap -m-8"
-				>
-					{contentGrid?.length > 0 ? (
-						contentGrid?.map((item, keys) => (
-							<Fragment key={keys}>
-								<TitleThreeContentGridCard
-									key={keys}
-									icon={item?.card?.icon}
-									title={item?.card?.title}
-									paragraph={item?.card?.paragraph}
-									buttonLink={item?.card?.buttonLink}
-								/>
-							</Fragment>
-						))
-					) : (
-						<></>
-					)}
-				</motion.div>
+						{contentGrid?.length > 0 ? (
+							contentGrid?.map((item, keys) => (
+								<Fragment key={keys}>
+									<TitleThreeContentGridCard
+										key={keys}
+										icon={item?.card?.icon}
+										title={item?.card?.title}
+										paragraph={item?.card?.paragraph}
+										buttonLink={item?.card?.buttonLink}
+									/>
+								</Fragment>
+							))
+						) : (
+							<></>
+						)}
+					</motion.div>
+				</div>
 			</div>
 		</div>
 	);
